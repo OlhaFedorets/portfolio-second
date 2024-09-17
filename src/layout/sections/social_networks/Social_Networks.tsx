573d360c32d688b1252d403f8f17d147617bf694
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-17 11:49:46 +0300

    create social_networks

---
+++ src/layout/sections/social_networks/Social_Networks.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import { Icon } from "../../../components/icon/Icon";
+
+
+export const SocialNetworks = () => {
+    return (
+        <NetworksIcon>
+            <ul>
+                <li>
+                    <a href=""><Icon iconId={"dribble"} width={"122"}/></a>
+                </li>
+                <li>
+                    <a href=""><Icon iconId={"linkedin"} width={"123"}/></a>
+                </li>
+                <li>
+                    <a href=""><Icon iconId={"medium"} width={"152"}/></a>
+                </li>
+                <li>
+                    <a href=""><Icon iconId={"behance"} width={"162"}/></a>
+                </li>
+            </ul>
+        </NetworksIcon>
+    );
+};
+
+
+const NetworksIcon = styled.nav`
+    background-color: lightgreen;
+    border: 1px solid red;
+
+    ul {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 150px;
+    }
+`
