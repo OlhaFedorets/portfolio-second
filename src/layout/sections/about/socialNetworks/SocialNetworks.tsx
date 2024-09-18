01eac015a1f901b64175f64624ea146b0540b458
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-18 12:01:03 +0300

    change social

---
+++ src/layout/sections/about/socialNetworks/SocialNetworks.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import { Icon } from "../../../../components/icon/Icon";
+
+
+
+export const SocialNetworks = () => {
+    return (
+        <StyledSocialNetworks>
+            <ul>
+                <li>
+                    <a href=""><Icon iconId={"dribble"} width={"122"}/></a>
+                </li>
+                <li>
+                    <a href=""><Icon iconId={"linkedin"} width={"123"}/></a>
+                </li>
+                <li>
+                    <a href=""><Icon iconId={"medium"} width={"152"}/></a>
+                </li>
+                <li>
+                    <a href=""><Icon iconId={"behance"} width={"162"}/></a>
+                </li>
+            </ul>
+        </StyledSocialNetworks>
+    );
+};
+
+
+const StyledSocialNetworks = styled.nav`
+    
+    background-color: #bbf3bb;
+    
+    ul {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 150px;
+    }
+`
