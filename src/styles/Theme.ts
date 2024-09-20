30de161a7d74cee281cdd715b101103e6590e3f3
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-20 12:16:53 +0300

    create theme

---
+++ src/styles/Theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+    colors: {
+        gradientAccent: "background: linear-gradient(214.02deg, #B75CFF 6.04%, #671AE4 92.95%)",
+        
+        gradientBG: "linear-gradient(214.02deg, rgba(183, 92, 255, 0.5) 6.04%, rgba(103, 26, 228, 0.5) 92.95%)",
+                
+        secondaryBg: "#181E41",
+
+        white: "#FFFFFF",
+        black: "#000000"
+    }
+}
