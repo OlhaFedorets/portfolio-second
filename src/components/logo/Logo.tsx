bbb2cb70696d9c2c6a7cbcb444c0ba0cfc3d3965
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-23 18:10:18 +0300

    change logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { theme } from "../../styles/Theme"
 
 
 
@@ -17,5 +18,17 @@ const StyledLogo = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    gap: 17px;
+    gap: 5px;
+    
+
+    a {
+        display: inline-block;
+        width: 40px;
+        height: 40px;        
+        border-radius: 50%;
+        background-image: ${theme.colors.gradientAccent};
+        color: ${theme.colors.white};
+        text-align: center;
+        line-height: 40px;
+    }
 `
