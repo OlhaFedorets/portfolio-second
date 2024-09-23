f15a6819ab6dd52a34076ae0b2ab658d206cef02
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-23 18:10:53 +0300

    change main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,27 +2,31 @@ import React from "react";
 import styled from "styled-components";
 import photo from "./../../../assets/images/home.jpg"
 import { FlexContainer } from "../../../components/FlexContainer";
+import { Container } from "../../../components/Container";
+import { theme } from "../../../styles/Theme";
 
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexContainer direction={"column"} align={"center"} justify={"space-around"}>
-                <MainTitle>
-                    I’m Alea. I will create a web design that can give value to your company
-                </MainTitle>
-                <PhotoWrapper>
-                    <div id="ui">
-                        <span>UI/UX Designer</span>
-                        <span>4 Years</span>
-                    </div>
-                    <Photo src={photo} />
-                    <div id="web">
-                        <span>Web Developer</span>
-                        <span>2 Years</span>
-                    </div>
-                </PhotoWrapper>
-            </FlexContainer>
+            <Container>
+                <FlexContainer direction={"column"} align={"center"} justify={"space-around"}>
+                    <MainTitle>
+                        I’m <span>Alea</span>. I will create a web design that can give value to your company
+                    </MainTitle>
+                    <PhotoWrapper>
+                        <div id="ui">
+                            <span>UI/UX Designer</span>
+                            <span>4 Years</span>
+                        </div>
+                        <Photo src={photo} />
+                        <div id="web">
+                            <span>Web Developer</span>
+                            <span>2 Years</span>
+                        </div>
+                    </PhotoWrapper>
+                </FlexContainer>
+            </Container>
         </StyledMain>
     );
 };
@@ -30,12 +34,22 @@ export const Main = () => {
 
 const StyledMain = styled.section`
     min-height: 100vh;
-    background-color: lightgrey;
+    display: flex;
+    margin-top: 90px;
 `
 
 const MainTitle = styled.h1`
-    
+    font-family: "Poppins", sans-serif;
+    font-weight: 700;
+    font-size: 50px;
+    //line-height: 120%;
+    //letter-spacing: 0.01em;
+    text-align: center;  
+    color: ${theme.colors.black};
 
+    span {
+        color: ${theme.colors.gradientAccent};
+    }
 `
 
 const PhotoWrapper = styled.div`
@@ -49,15 +63,40 @@ const PhotoWrapper = styled.div`
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        border: 1px solid black;
-    }
+        
+        border-radius: 200px;
+        width: 288px;
+        height: 100px;
+        background-image: ${theme.colors.gradientAccent};
+        
+        span{
+            
+        :first-child {
+            font-family: 'Poppins', sans-serif;
+            font-weight: 700;
+            font-size: 20px;
+            color: ${theme.colors.white};
+        }
+
+         :last-child {
+            font-family: 'Poppins', sans-serif;
+            font-weight: 500;
+            font-size: 15px;
+            color: ${theme.colors.white};
+            }
+        }
+        }
+            
+    
 
     #ui {
     align-self: flex-start;
+    margin-top: 50px;
     }
 
     #web {
     align-self: flex-end;
+    margin-bottom: 100px;
     }
 `
 
