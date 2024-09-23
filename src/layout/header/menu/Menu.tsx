bfdb6dc294fcc62ae9a889b035a13db6b1c62439
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-23 18:10:44 +0300

    change menu

--- src/layout/header/menu/Menu.tsx
+++ src/layout/header/menu/Menu.tsx
@@ -23,13 +23,30 @@ export const Menu = (props: { menuItems: Array<string> }) => {
 const StyledMenu = styled.nav`
     ul {
     display: flex;
-    gap: 40px;    
+    align-items: center;
+    gap: 40px;
+
+
+    li:last-child a {
+        display: block;
+        width: 134px;
+        height: 50px;
+        border-radius: 100px;
+        padding: 10px 20px;       
+        background-image: ${theme.colors.gradientBG};
+    }
     }
 `
 
 const ListItem = styled.li`
-    color: ${theme.colors.white};
+    
 `
 const Link = styled.a`
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-size: 20px;
+    text-align: center;
+    color: ${theme.colors.white};
+    
     
 `
