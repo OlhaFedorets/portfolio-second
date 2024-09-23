772edebfa16c3e08dad5d253027380c3a2213594
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-23 18:11:12 +0300

    change socialNetwork

--- src/layout/sections/socialNetworks/SocialNetworks.tsx
+++ src/layout/sections/socialNetworks/SocialNetworks.tsx
@@ -4,36 +4,38 @@ import { Icon } from "../../../components/icon/Icon";
 
 
 
+
 export const SocialNetworks = () => {
     return (
         <StyledSocialNetworks>
             <ul>
                 <li>
-                    <a href=""><Icon iconId={"dribble"} width={"122"}/></a>
+                    <a href=""><Icon iconId={"dribble"}  width={"123"} height={"38"} viewBox={"0 0 130 38"}/></a>
                 </li>
                 <li>
-                    <a href=""><Icon iconId={"linkedin"} width={"123"}/></a>
+                    <a href=""><Icon iconId={"linkedin"} width={"132"} height={"38"} viewBox={"0 0 132 38"}/></a>
                 </li>
                 <li>
-                    <a href=""><Icon iconId={"medium"} width={"152"}/></a>
+                    <a href=""><Icon iconId={"medium"} width={"161"} height={"30"} viewBox={"0 0 161 38"}/></a>
                 </li>
                 <li>
-                    <a href=""><Icon iconId={"behance"} width={"162"}/></a>
+                    <a href=""><Icon iconId={"behance"} width={"162"} height={"38"} viewBox={"0 0 167 38"}/></a>
                 </li>
-            </ul>
+            </ul>                      
         </StyledSocialNetworks>
     );
 };
 
 
 const StyledSocialNetworks = styled.section`
-    
-    background-color: #bbf3bb;
-    
+        
     ul {
         display: flex;
         justify-content: center;
         align-items: center;
-        gap: 150px;
+        gap:150px;
+        //height: 100%;
+        //min-height: 135px;
+        padding: 55px 0 50px;
     }
 `
