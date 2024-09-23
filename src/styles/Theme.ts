0678aa93929d5a3300d54e1712b4ce1f24fb1eb1
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-23 18:11:28 +0300

    change theme

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,6 +1,6 @@
 export const theme = {
     colors: {
-        gradientAccent: "background: linear-gradient(214.02deg, #B75CFF 6.04%, #671AE4 92.95%)",
+        gradientAccent: "linear-gradient(214.02deg, #B75CFF 6.04%, #671AE4 92.95%)",
         
         gradientBG: "linear-gradient(214.02deg, rgba(183, 92, 255, 0.5) 6.04%, rgba(103, 26, 228, 0.5) 92.95%)",
                 
