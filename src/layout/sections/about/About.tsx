b5472822cce1e057be0bf5735596412e2a2734f6
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-24 15:12:11 +0300

    change about

--- src/layout/sections/about/About.tsx
+++ src/layout/sections/about/About.tsx
@@ -2,41 +2,53 @@ import React from "react";
 import styled from "styled-components";
 import { FlexContainer } from "../../../components/FlexContainer";
 import { PersonalAchievements } from "./personalAchievements/PersonalAchievements"
+import { Container } from "../../../components/Container";
+import { theme } from "../../../styles/Theme";
 
-
+const achievementItems = ["Projects Done", "Happy Clients", "Clients on Worldwide"]
 
 
 export const About = () => {
     return (
         <StyledAbout>
-            <FlexContainer direction={"column"}>
-                <StyledPersonalInfo>
-                    <StyledPersonalTitle>About Me.</StyledPersonalTitle>
-                    <StyledPersonalText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.</StyledPersonalText>
-                </StyledPersonalInfo>
-                <PersonalAchievements />
-            </FlexContainer>
+            <Container>
+                <FlexContainer direction={"column"}>
+                    <StyledPersonalInfo>
+                        <StyledPersonalTitle>About Me.</StyledPersonalTitle>
+                        <StyledPersonalText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.</StyledPersonalText>
+                    </StyledPersonalInfo>
+                    <PersonalAchievements  achievementItems={achievementItems}/>
+                </FlexContainer>
+            </Container>
         </StyledAbout>
     );
 };
 
 
 const StyledAbout = styled.section`
-    
-    min-height: 100vh;
-    
+    display: flex;
 `
 const StyledPersonalInfo = styled.div`
         display: flex;
         justify-content: space-between;
-        align-items: center;
+        align-items: center;        
 `
 
 const StyledPersonalTitle = styled.h2`
-    
+    flex: 1;
+    font-family: "Poppins", sans-serif;
+    font-weight: 700;
+    font-size: 100px;
+    line-height: 120%;
+    color: ${theme.colors.white};
 `
 
 const StyledPersonalText = styled.p`
-    max-width: 793px
+    flex: 2;
+    max-width: 793px;
+    font-family: "Poppins", sans-serif;
+    font-weight: 500;
+    font-size: 30px;
+    color: rgba(255, 255, 255, 0.5);
 `
 
