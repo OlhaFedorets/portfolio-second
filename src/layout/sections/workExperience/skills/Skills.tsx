074e076315c7eec1d5b232806acccfd467a15bd0
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-24 15:13:56 +0300

    change skills

--- src/layout/sections/workExperience/skills/Skills.tsx
+++ src/layout/sections/workExperience/skills/Skills.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import photo from "./../../../../assets/images/home.webp";
 import { Indicator } from "./indicator/Indicator";
+import { theme } from "../../../../styles/Theme";
 
 
 
@@ -13,8 +14,10 @@ export const Skills = () => {
            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</p>
            <BrandName>
                 <Photo src={photo}/>
+                <div>
                 <span>Alea Angelina</span>
                 <span>Web Designer</span>
+                </div>                
            </BrandName>
            <Indicator percent={"+52.5%"} title={"Task finished"}/>
            <Indicator percent={"+67.5%"} title={"Progress speed"}/>
@@ -24,16 +27,64 @@ export const Skills = () => {
 
 
 const StyledSkills = styled.div`
+    max-width: 461px;
+    font-family: "Poppins", sans-serif;
+    color: ${theme.colors.black};
+    margin-left: 145px;
 
+    h3 {
+        
+        font-weight: 600;
+        font-size: 40px;
+        line-height: 125%;
+        letter-spacing: 0.01em;
+        
+    }
+
+    p {
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 180%;
+        letter-spacing: 0.02em;
+        color: rgba(0, 0, 0, 0.5);
+        margin: 10px 0 40px;
+    }
 `
 
-const BrandName = styled.div`
-max-width: 259px;
 
+
+const BrandName = styled.div`
+    display: flex;
+    
+
+    /* max-width: 259px; */
+    div {
+        display: flex;
+        flex-direction: column;
+        margin-left: 16px;
+
+        span {
+           font-weight: 500;
+            font-size: 20px;
+            /* line-height: 180%;
+            letter-spacing: 0.02em; */
+        }
+
+        span:last-child {
+            font-weight: 300;
+            font-size: 15px;
+            /* line-height: 180%;
+            letter-spacing: 0.02em; */
+        }
+    }
 `
 
 const Photo = styled.img`
-max-width: 50px`
+width: 50px;
+height: 50px;
+border-radius: 50%;
+object-fit: cover;
+`
 
 
 
