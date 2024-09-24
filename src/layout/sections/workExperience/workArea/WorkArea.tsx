0f6c154178a5dc968396805a415a7635b5a19c6d
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-24 15:14:18 +0300

    change workArea

--- src/layout/sections/workExperience/workArea/WorkArea.tsx
+++ src/layout/sections/workExperience/workArea/WorkArea.tsx
@@ -1,36 +1,63 @@
 import React from "react";
 import styled from "styled-components";
 import { Icon } from "../../../../components/icon/Icon";
+import { theme } from "../../../../styles/Theme";
 
 type WorkAreaPropsType = {
     iconId: string,
+    width: string,
+    height: string,
+    viewBox: string,
     title: string,
-    text: string    
+    text: string
 }
 
 
 export const WorkArea = (props: WorkAreaPropsType) => {
     return (
         <StyledWorkArea>
-           <Icon iconId={props.iconId}/>
-           <WorkTitle>{props.title}</WorkTitle>
-           <WorkText>{props.text}</WorkText>
+            <WorkIcon>
+                <Icon iconId={props.iconId} width={props.width} height={props.height} viewBox={props.viewBox} />
+            </WorkIcon>
+            <WorkTitle>{props.title}</WorkTitle>
+            <WorkText>{props.text}</WorkText>
         </StyledWorkArea>
     )
 }
 
 
 const StyledWorkArea = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-background-color: #fbc1cb;
-max-width: 540px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 288px;
+    height: 460px;
+    padding: 100px 20px;
+    color: ${theme.colors.white};
+    box-shadow: 0 5px 4px 0 rgba(0, 0, 0, 0.25);
+    background-image:${theme.colors.gradientAccent};
+    border-radius: 30px;
+    margin-right: 15px;
 `
 
-const WorkIcon = styled.img``
+const WorkIcon = styled.div`
+vertical-align: middle
+`
 
-const WorkTitle = styled.h3``
+const WorkTitle = styled.h3`
+font-family: "Poppins", sans-serif;
+font-weight: 700;
+font-size: 30px;
+line-height: 120%;
+text-align: center;
+margin: 100px 0 10px;
+`
 
-const WorkText = styled.p``
+const WorkText = styled.p`
+font-family: "Poppins", sans-serif;
+font-weight: 500;
+font-size: 20px;
+line-height: 120%;
+text-align: center;
+color: rgba(255, 255, 255, 0.5);
+`
