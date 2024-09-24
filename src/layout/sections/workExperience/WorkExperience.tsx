e96bfcac56c9e12a72b4cf0e62942cd94107fcda
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-24 15:13:41 +0300

    change workExperience

--- src/layout/sections/workExperience/WorkExperience.tsx
+++ src/layout/sections/workExperience/WorkExperience.tsx
@@ -3,27 +3,29 @@ import styled from "styled-components";
 import { FlexContainer } from "../../../components/FlexContainer";
 import { WorkArea } from "./workArea/WorkArea";
 import { Skills } from "../workExperience/skills/Skills";
+import { Container } from "../../../components/Container";
 
 
 
 export const WorkExperience = () => {
     return (
         <StyledWorkExperience>
-            <FlexContainer>
-                <WorkArea iconId={"computer"} title={"Web Design"} 
-                text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."} />
-                <WorkArea iconId={"paint"} title={"UI/UX Design"} 
-                text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."} />
-                <Skills />
-            </FlexContainer>
+            <Container>
+                <FlexContainer>
+                    <WorkArea iconId={"computer"} height={"40"} width={"41"} viewBox={"0 0 28 30"}
+                        title={"Web Design"}
+                        text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."} />
+                    <WorkArea iconId={"paint"} height={"40"} width={"41"} viewBox={"0 0 39 40"}
+                        title={"UI/UX Design"}
+                        text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."} />
+                    <Skills />
+                </FlexContainer>
+            </Container>
         </StyledWorkExperience>
     )
 }
 
 
 const StyledWorkExperience = styled.section`
-min-height: 100vh;
-    background-color: rgb(240, 184, 247);
-
-
+      
 `
