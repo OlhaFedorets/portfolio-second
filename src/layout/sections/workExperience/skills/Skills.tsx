aa0bd7cb49c71bec18387fadbe965f1eaca4f292
OlhaFedorets <fedoretsolha@gmail.com>
2024-10-03 15:02:44 +0300

    change skills

--- src/layout/sections/workExperience/skills/Skills.tsx
+++ src/layout/sections/workExperience/skills/Skills.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import photo from "./../../../../assets/images/home.webp";
 import { Indicator } from "./indicator/Indicator";
 import { theme } from "../../../../styles/Theme";
+import { font } from "../../../../styles/Common"
 
 
 
@@ -10,8 +11,10 @@ import { theme } from "../../../../styles/Theme";
 export const Skills = () => {
     return (
         <StyledSkills>
+            <div>
             <h3>What I Can Do To Help Build Your Product</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</p>
+            </div>
             <BrandName>
                 <Photo src={photo} />
                 <div>
@@ -31,47 +34,43 @@ export const Skills = () => {
 const StyledSkills = styled.div`
     max-width: 461px;
     color: ${theme.colors.black};
-    margin-left: 145px;
-
+    
     h3 {
-        font-weight: 600;
-        font-size: 40px;
+        ${font({weight: 600, Fmax: 40, Fmin: 29})};
         line-height: 125%;
         letter-spacing: 0.01em;        
     }
 
     p {
-        font-weight: 400;
-        font-size: 20px;
+        ${font({weight: 400, Fmax: 20, Fmin: 16})};
         line-height: 180%;
         letter-spacing: 0.02em;
         color: rgba(0, 0, 0, 0.5);
         margin: 10px 0 40px;
     }
+
+    /* @media screen and (max-width: 1200px) {
+        display: flex;
+        max-width: 1000px;
+
+    } */
 `
 
 const BrandName = styled.div`
     display: flex;
     
 
-    /* max-width: 259px; */
-    div {
+        div {
         display: flex;
         flex-direction: column;
         margin-left: 16px;
 
         span {
-           font-weight: 500;
-            font-size: 20px;
-            /* line-height: 180%;
-            letter-spacing: 0.02em; */
+            ${font({weight: 500, Fmax: 20, Fmin: 16})}
         }
 
         span:last-child {
-            font-weight: 300;
-            font-size: 15px;
-            /* line-height: 180%;
-            letter-spacing: 0.02em; */
+            ${font({weight: 300, Fmax: 15, Fmin: 12})}
         }
     }
 `
@@ -95,7 +94,8 @@ const Indicators = styled.div`
         content: "";
         display: inline-block;
         border: 1px solid #000;
-        width: 420px;
+        max-width: 420px;
+        width: 100%;
         height: 0px;
         top: -40px;
 
