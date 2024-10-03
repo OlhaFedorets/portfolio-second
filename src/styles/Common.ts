aee0f5f63a7ebfb95084b4be6e995ab215b20659
OlhaFedorets <fedoretsolha@gmail.com>
2024-10-03 15:03:24 +0300

    change common

--- src/styles/Common.ts
+++ src/styles/Common.ts
@@ -10,5 +10,5 @@ export const font = ({family, weight, lineHeight, Fmin, Fmax}: FontPropsType) =>
     font-family: ${family || 'Poppins'};
     font-weight: ${weight || 400};
     lineHeight: ${lineHeight || 1.2};
-    font-size: calc( (100vw - 360px) / (1440 - 360) * (${Fmax} - ${Fmin}) + ${Fmin}px);
+    font-size: clamp(${Fmin}px, calc( (100vw - 360px) / (1440 - 360) * (${Fmax} - ${Fmin}) + ${Fmin}px), ${Fmax}px);
 ` 
