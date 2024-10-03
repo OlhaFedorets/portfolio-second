1831541e5414481ad2c2d7f4828d722163fcc3f3
OlhaFedorets <fedoretsolha@gmail.com>
2024-10-03 15:02:22 +0300

    workExperience

--- src/layout/sections/workExperience/WorkExperience.tsx
+++ src/layout/sections/workExperience/WorkExperience.tsx
@@ -4,6 +4,7 @@ import { FlexContainer } from "../../../components/FlexContainer";
 import { WorkArea } from "./workArea/WorkArea";
 import { Skills } from "../workExperience/skills/Skills";
 import { Container } from "../../../components/Container";
+import { theme } from "../../../styles/Theme";
 
 
 
@@ -11,13 +12,15 @@ export const WorkExperience = () => {
     return (
         <StyledWorkExperience>
             <Container>
-                <FlexContainer>
+                <FlexContainer justify={"space-between"}>
+                    <WorkAreas>                    
                     <WorkArea iconId={"computer"} height={"40"} width={"41"} viewBox={"0 0 28 30"}
                         title={"Web Design"}
                         text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."} />
                     <WorkArea iconId={"paint"} height={"40"} width={"41"} viewBox={"0 0 39 40"}
                         title={"UI/UX Design"}
                         text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit."} />
+                    </WorkAreas>
                     <Skills />
                 </FlexContainer>
             </Container>
@@ -25,7 +28,23 @@ export const WorkExperience = () => {
     )
 }
 
-
+ 
 const StyledWorkExperience = styled.section`
-      
+    ${FlexContainer} {
+        @media ${theme.media.tablet} {
+            flex-direction: column;
+            align-items: center;
+            gap: 20px;
+        }
+    }
+`
+
+const WorkAreas = styled.div`
+    display: flex;
+    justify-content: center;
+
+    @media ${theme.media.mobile} {
+        flex-direction: column;
+        gap: 20px;
+    }
 `
