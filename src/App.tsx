e563950fd8b2a52f3041b9c3ccd57b9b810ec395
OlhaFedorets <fedoretsolha@gmail.com>
2025-07-12 10:19:32 +0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css';
 import styled from 'styled-components';
-import { Header } from './layout/header/Header';
-import { Main } from './layout/sections/main/Main';
-import { SocialNetworks } from './layout/sections/socialNetworks/SocialNetworks';
-import { About } from './layout/sections/about/About';
-import { WorkExperience } from './layout/sections/workExperience/WorkExperience';
-import { Testimony } from './layout/sections/testimony/Testimony';
-import { Contacts } from './layout/sections/contacts/Contacts';
-import { Footer } from './layout/footer/Footer';
+import {Header} from './layout/header/Header';
+import {Main} from './layout/sections/main/Main';
+import {SocialNetworks} from './layout/sections/socialNetworks/SocialNetworks';
+import {About} from './layout/sections/about/About';
+import {WorkExperience} from './layout/sections/workExperience/WorkExperience';
+import {Testimony} from './layout/sections/testimony/Testimony';
+import {Contacts} from './layout/sections/contacts/Contacts';
 import {Particle} from "./components/particle/Particle";
 
 function App() {
@@ -21,15 +20,8 @@ function App() {
             <WorkExperience/>
             <Testimony/>
             <Contacts/>
-            <Footer/>
         </div>
     );
 }
 
 export default App;
-
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
