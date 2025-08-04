135748ab132f2fa00171122bdece250825a9d90b
OlhaFedorets <fedoretsolha@gmail.com>
2025-08-04 15:31:10 +0300

    change desktopMenu

---
+++ src/layout/header/desktopMenu/DesktopMenu.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../../../styles/Theme";
+import {Menu} from "../menu/Menu";
+
+
+export const DesktopMenu = () => {
+    return (
+        <StyledDesktopMenu>
+            <Menu/>
+        </StyledDesktopMenu>
+    );
+};
+
+
+const StyledDesktopMenu = styled.nav`
+    ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 40px;
+
+
+    li:last-child a {
+        display: block;
+        width: 134px;
+        height: 50px;
+        border-radius: 100px;
+        padding: 10px 20px;       
+        background-image: ${theme.colors.gradientBG};
+        }
+    }
+
+    @media ${theme.media.tablet} {
+        display: none;
+    }
+`
+
