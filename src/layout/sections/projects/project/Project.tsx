1f43846e0f76095ed313c8f960b56c2cf5c8cbb5
OlhaFedorets <fedoretsolha@gmail.com>
2025-08-12 13:17:16 +0300

    change project

---
+++ src/layout/sections/projects/project/Project.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import styled from "styled-components";
+import {Link} from "../../../../components/Link";
+import {Button} from "../../../../components/Button";
+import {theme} from "../../../../styles/Theme";
+import {Zoom} from "react-awesome-reveal";
+
+
+type WorkPropsType = {
+    title: string
+    text: string
+    src: string
+}
+
+export const Project: React.FC<WorkPropsType> = (props: WorkPropsType) => {
+    return (
+        <StyledProject>
+            <Zoom damping={0.1}>
+            <StyledImageWrapper>
+                <StyledImage src={props.src} alt=""/>
+                <Button width={'200px'} height={'40px'}>
+                    <a href={''}>View Project</a>
+                </Button>
+            </StyledImageWrapper>
+            <StyledDescription>
+                <StyledTitle>{props.title}</StyledTitle>
+                <StyledText>{props.text}</StyledText>
+            </StyledDescription>
+            </Zoom>
+        </StyledProject>
+    );
+};
+
+
+
+
+const StyledProject = styled.div`
+    width: 400px;
+    flex-grow: 1;
+    max-width: 540px;
+    background-color: ${theme.colors.secondaryBg};
+
+    ${Link} {
+        padding: 10px 0;
+
+        & + ${Link} {
+            margin-left: 20px;
+        }
+    }
+`
+
+const StyledImageWrapper = styled.div`
+    position: relative;
+
+    ${Button} {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -40%);
+        transition: ${theme.animations.transition};
+    }
+
+    &::before {
+        content: "";
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        background: rgba(0, 0, 0, 0.3);
+        backdrop-filter: blur(2px);
+        opacity: 0;
+        transition: ${theme.animations.transition};
+    }
+
+    &:hover {
+        &::before {
+            opacity: 1;
+        }
+    }
+    
+    @media ${theme.media.tablet} {
+        &::before {
+            opacity: 1;
+        }
+
+        ${Button} {
+            opacity: 1;
+        }
+    }
+`
+
+const StyledImage = styled.img`
+    width: 100%;
+    height: 260px;
+    object-fit: cover;
+`
+
+const StyledDescription = styled.div`
+    padding: 25px 20px;
+`
+
+const StyledTitle = styled.h3`
+
+`
+
+const StyledText = styled.p`
+    margin: 14px 0 10px;
+`
