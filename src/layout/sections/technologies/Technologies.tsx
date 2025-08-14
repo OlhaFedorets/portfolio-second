51dba81aaa7efd1ce40d57d31abd3043bf802778
OlhaFedorets <fedoretsolha@gmail.com>
2025-08-14 12:45:25 +0300

    change technologies

---
+++ src/layout/sections/technologies/Technologies.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import styled from "styled-components";
+import {Icon} from "../../../components/icon/Icon";
+import {theme} from "../../../styles/Theme";
+import {Container} from "../../../components/Container";
+import {FlexContainer} from "../../../components/FlexContainer";
+
+const technologiesData = [
+    {
+        iconId: "js1",
+        viewBox: "0 0 16 16"
+    },
+    {
+        iconId: "ts1",
+        viewBox: "0 0 16 16"
+    },
+    {
+        iconId: "react1",
+        viewBox: "0 0 16 16"
+    },
+    {
+        iconId: "redux1",
+        viewBox: "2 2 20 20"
+    },
+]
+
+
+export const Technologies = () => {
+    return (
+        <StyledTechnologies>
+            <Container>
+                <FlexContainer justify={"space-between"} align={"center"} wrap={"wrap"}>
+                    {technologiesData.map((t, index) => {
+                        return (
+                            <StyledTechnology>
+                                <Icon
+                                    iconId={t.iconId}
+                                    width={"100"}
+                                    height={"100"}
+                                    viewBox={t.viewBox}
+                                    key={index}/>
+                            </StyledTechnology>
+                        )
+                    })}
+                </FlexContainer>
+            </Container>
+        </StyledTechnologies>
+    );
+};
+
+
+const StyledTechnologies = styled.section`
+    padding: 30px;
+    
+        // ${FlexContainer} {
+        // @media screen and (max-width: 600px) {
+        //     flex-direction: column;
+        //     gap: 15px;
+        // }
+    }
+`
+const StyledTechnology = styled.div`
+    color: rgba(255, 255, 255, 0.5);
+    transition: color 0.3s, transform 0.3s;
+
+    &:hover {
+        color: ${theme.colors.white};
+    }
+
+     @media screen and (max-width: 600px) {
+         display: none;
+     }
+
+`
