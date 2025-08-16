9ce06d59b2273e3d5c04d50de9bcf983d2bc73b2
OlhaFedorets <fedoretsolha@gmail.com>
2025-08-16 22:13:29 +0300

    change about

--- src/layout/sections/about/About.tsx
+++ src/layout/sections/about/About.tsx
@@ -4,47 +4,41 @@ import {Container} from "../../../components/Container";
 import {theme} from "../../../styles/Theme";
 import {font} from "../../../styles/Common";
 import Typewriter from 'typewriter-effect';
-
-// const achievementItems = ["Projects Done", "Happy Clients", "Clients on Worldwide"]
+import {FlexContainer} from "../../../components/FlexContainer";
 
 
 export const About = () => {
     return (
         <StyledAbout id={"about"}>
             <Container>
-                {/*<FlexContainer direction={"column"}>*/}
-                    <StyledPersonalInfo>
-                        {/*<StyledPersonalTitle>About Me.</StyledPersonalTitle>*/}
-                        <StyledPersonalTitle>
-                            {/*About me*/}
-                            <Typewriter
-                                options={{
-                                    strings: ['About', 'Me.'],
-                                    autoStart: true,
-                                    loop: true,
-                                }}
-                            />
-                        </StyledPersonalTitle>
-                        <StyledPersonalText>
-                            <p>
-                                Hello! My name is Olga. Frontend developer with 1+ years of experience in Single Page
-                                Application development using TypeScript, JavaScript, React, Redux, Redux-toolkit, RTK
-                                Query.
-                            </p>
-                            <p>
-                                I am constantly improving my skills in these technologies, and am also actively studying
-                                Next.js and React Native to expand my capabilities and achieve a higher level of
-                                professionalism in my work.
-                            </p>
-                            <p>
-                                My main development priority is to create efficient and clean code that is easy to
-                                maintain and scale. I strive to create high-quality, intuitive and accessible user
-                                interfaces.
-                            </p>
-                        </StyledPersonalText>
-                    </StyledPersonalInfo>
-                    {/*<PersonalAchievements achievementItems={achievementItems}/>*/}
-                {/*</FlexContainer>*/}
+                <FlexContainer justify={'space-between'} align={'center'} wrap={'wrap'}>
+                    <StyledPersonalTitle>
+                        <Typewriter
+                            options={{
+                                strings: ['About', 'Me.'],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </StyledPersonalTitle>
+                    <StyledPersonalText>
+                        <p>
+                            Hello! My name is Olga. Frontend developer with 1+ years of experience in Single Page
+                            Application development using TypeScript, JavaScript, React, Redux, Redux-toolkit, RTK
+                            Query.
+                        </p>
+                        <p>
+                            I am constantly improving my skills in these technologies, and am also actively studying
+                            Next.js and React Native to expand my capabilities and achieve a higher level of
+                            professionalism in my work.
+                        </p>
+                        <p>
+                            My main development priority is to create efficient and clean code that is easy to
+                            maintain and scale. I strive to create high-quality, intuitive and accessible user
+                            interfaces.
+                        </p>
+                    </StyledPersonalText>
+                </FlexContainer>
             </Container>
         </StyledAbout>
     );
@@ -54,19 +48,15 @@ export const About = () => {
 const StyledAbout = styled.section`
     position: relative;
     color: ${theme.colors.white};
-`
-const StyledPersonalInfo = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    align-items: center;
-    gap: 10px;
 
+    ${FlexContainer} {
+        gap: 10px;
 
-    @media screen and (max-width: 950px) {
-        flex-direction: column;
-        margin-left: 20px;
-        margin-right: 20px;
+        @media screen and (max-width: 1150px) {
+            flex-direction: column;
+            margin-left: 20px;
+            margin-right: 20px;
+        }
     }
 `
 
@@ -74,7 +64,6 @@ const StyledPersonalTitle = styled.h2`
     flex: 1;
     ${font({weight: 700, Fmax: 100, Fmin: 70})};
     line-height: 120%;
-
 `
 
 const StyledPersonalText = styled.div`
@@ -82,7 +71,6 @@ const StyledPersonalText = styled.div`
     flex-direction: row;
     flex-wrap: wrap;
     gap: 20px;
-    //flex: 2;    
     max-width: 793px;
     min-width: 325px;
     ${font({weight: 400, Fmax: 28, Fmin: 20})};
